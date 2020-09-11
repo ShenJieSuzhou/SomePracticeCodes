7c9ea385d3c60dd327d75cb0d95f5b8ad5a82fd5
ShenJieSuzhou <745689122@qq.com>
2020-09-11 17:10:16 +0800

    搭建SB框架

---
+++ SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SimpleNeteaseMusic
+//
+//  Created by shenjie on 2020/9/11.
+//  Copyright © 2020 shenjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    @IBOutlet weak var appTabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
---
+++ SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController/JJCustomNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  JJCustomNavigationController.swift
+//  SimpleNeteaseMusic
+//
+//  Created by shenjie on 2020/9/11.
+//  Copyright © 2020 shenjie. All rights reserved.
+//
+
+import UIKit
+
+class JJCustomNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
