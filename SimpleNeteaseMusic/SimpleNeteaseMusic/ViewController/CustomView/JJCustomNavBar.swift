108a2d2d1906a7ef8df3f6250fe3775b3fb78a22
Shenjie <745689122@qq.com>
2020-09-13 17:15:49 +0800

    顶部搜索栏
    顶部搜索栏构建

---
+++ SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController/CustomView/JJCustomNavBar.swift
@@ -0,0 +1,21 @@
+//
+//  JJCustomNavBar.swift
+//  SimpleNeteaseMusic
+//
+//  Created by snaigame on 2020/9/13.
+//  Copyright © 2020 shenjie. All rights reserved.
+//
+
+import UIKit
+
+class JJCustomNavBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController/DiscoveryViewController.swift
+++ SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController/DiscoveryViewController.swift
@@ -8,23 +8,50 @@
 
 import UIKit
 
-class DiscoveryViewController: UIViewController {
-
+class DiscoveryViewController: UITableViewController {
+    
+    //搜索
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // 设置搜索栏
+        setupSearchController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 设置搜索视图
+    func setupSearchController () {
+        let leftItem = UIBarButtonItem(image: UIImage(named: "icon-gmail")?.withRenderingMode(.alwaysOriginal), style: UIBarButtonItem.Style.plain, target: self, action: #selector(microphoneBtnClicked))
+        let rightItem = UIBarButtonItem(image: UIImage(named: "icon-hangouts")?.withRenderingMode(.alwaysOriginal), style: UIBarButtonItem.Style.plain, target: self, action: #selector(playingBtnClicked))
+        self.navigationItem.leftBarButtonItem = leftItem
+        self.navigationItem.rightBarButtonItem = rightItem
+        searchController.searchResultsUpdater = self
+        definesPresentationContext = true
+        searchController.searchBar.delegate = self
+        self.navigationItem.titleView = searchController.searchBar
     }
-    */
 
+    
+    @objc func microphoneBtnClicked(){
+        print("11111111")
+    }
+    
+    @objc func playingBtnClicked(){
+        print("22222222")
+    }
+}
+
+extension DiscoveryViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        
+    }
+    
+}
+
+extension DiscoveryViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+    }
 }
