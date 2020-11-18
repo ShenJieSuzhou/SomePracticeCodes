833c6464b6b49e92abd23f8da7d1c9775845b72d
ShenJieSuzhou <745689122@qq.com>
2020-11-18 15:17:19 +0800

    添加 snapkit, 首页 menus ui
    添加 snapkit, 首页 menus ui

---
+++ SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController/SubVIew/Menu/HomeMenu.swift
@@ -0,0 +1,13 @@
+//
+//  HomeMenu.swift
+//  SimpleNeteaseMusic
+//
+//  Created by shenjie on 2020/11/18.
+//  Copyright © 2020 shenjie. All rights reserved.
+//
+
+import UIKit
+
+class HomeMenu: UIView {
+    
+}
--- SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController/SubVIew/Menu/HomeMenuCell.swift
+++ SimpleNeteaseMusic/SimpleNeteaseMusic/ViewController/SubVIew/Menu/HomeMenuCell.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2020 shenjie. All rights reserved.
 //
 
+import UIKit
 import Foundation
+
+class HomeMenuCell: UICollectionViewCell {
+    var menuIcon = UIImageView()
+    var menuText = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.addSubview(self.menuIcon)
+        self.contentView.addSubview(self.menuText)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupUI(imageName: String, title: String) -> Void {
+        
+    }
+}
+
